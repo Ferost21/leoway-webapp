950bec4ab2ee05f9cda272cd6fd50aaef9ee6fe1
Ferost <87854065+FerostUA@users.noreply.github.com>
2025-07-09 13:23:11 +0300

    Add frontend files: index.html, styles.css, script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let isModalOpen = false;
 let isDriverRideModalOpen = false;
 let currentPage = 'search';
 
-const API_BASE_URL = 'https://f51fab9daa2b.ngrok-free.app';
+const API_BASE_URL = 'https://a989a3640a73.ngrok-free.app';
 
 // Функція для оновлення стилів на основі теми
 function updateTheme() {
