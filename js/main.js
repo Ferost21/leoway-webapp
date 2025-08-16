314acf900d8e036309a257ef78f7e64ba6f3e969
Ferost <87854065+FerostUA@users.noreply.github.com>
2025-08-16 11:05:20 +0300

    Add frontend

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let isModalOpen = false;
 let isDriverRideModalOpen = false;
 let currentPage = 'search';
 
-const API_BASE_URL = 'https://cb42119b46c9.ngrok-free.app';
+const API_BASE_URL = 'https://1bbb7d907bd4.ngrok-free.app';
 
 function updateTheme() {
     const themeParams = webApp.themeParams || {};
